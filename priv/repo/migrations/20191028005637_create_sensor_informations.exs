0aa8a815b3d76a8159a3f401a0d97569897d4149
Paulo Daniel Carneiro <paulodaniel@usp.br>
2019-11-16 17:51:12 -0300

    change in sensor information schema in bd

--- priv/repo/migrations/20191028005637_create_sensor_informations.exs
+++ priv/repo/migrations/20191028005637_create_sensor_informations.exs
@@ -3,19 +3,9 @@ defmodule Scenia.Repo.Migrations.CreateSensorInformations do
 
   def change do
     create table(:sensor_informations) do
-      add :A1, :float
-      add :A2, :float
-      add :A3, :float
-      add :freq1, :float
-      add :freq2, :float
-      add :freq3, :float
-      add :Irms, :float
-      add :Vrms, :float
-      add :module_serial, references(:module, on_delete: :nothing)
+      add :value, :float
 
       timestamps()
     end
-
-    create index(:sensor_informations, [:module_serial])
   end
 end
